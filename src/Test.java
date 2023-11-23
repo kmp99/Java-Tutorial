f7a40eaff9d28145dc01abd9430804ac4cd9aeab
Krishan <kmpsingh1993@gmail.com>
2023-11-23 22:37:34 +0100

    stash changes

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,12 @@
 public class Test {
     public static void main(String[] args)
     {
+<<<<<<< Updated upstream
         byte b = 128;
         //changes for ABC-132
+=======
+        byte b = 127;
+>>>>>>> Stashed changes
         System.out.println(b);
     }
 }
